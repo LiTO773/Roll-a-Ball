29163488d39cb6cc81291176ef6c7b965e012bad
Luís Pinto <luispinto2000@gmail.com>
2015-02-21 11:32:25 +0000

    Player now moves

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical); //X Y Z
+
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+
+
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
